9a5225d8021cbce0fba51cadecb5ad5f1697bd88
haroldkim22 <haroldkkiimm22@sasa.hs.kr>
2026-06-09 17:44:37 +0900

    Update FloorMap.tsx

--- src/components/FloorMap.tsx
+++ src/components/FloorMap.tsx
@@ -108,7 +108,7 @@ export const FloorMap = ({
         initialScale={1}
         minScale={1}
         maxScale={5}
-        wheel={{ step: 0.15 }}
+        wheel={{ step: 0.05 }}
         doubleClick={{ disabled: true }}
         panning={{ disabled: false, velocityDisabled: true }}
       >
